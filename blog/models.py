7162975c1ef4ce26ba8da885b1b301b934396053
이수민 <min001017@naver.com>
2021-07-07 00:08:47 +0900

    Post model created, automatically save time

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.fields.related import create_many_to_many_intermediary_model
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
+    #author: 추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
