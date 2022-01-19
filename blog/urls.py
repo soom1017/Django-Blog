f00e1fd91b478318da4f4e2f40005c71ee8823e5
soomin <min001017@naver.com>
2022-01-19 15:48:48 +0900

    blog 개발 잠시 중단

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
+    path('create_post/', views.PostCreate.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
